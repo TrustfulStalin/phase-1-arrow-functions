f5650938aaecda439ee2178be34cfecc2279884d
Tevon Talbert <TevonTalbert@hotmail.com>
2023-10-03 15:40:25 -0500

    adding file

---
+++ index.js
@@ -0,0 +1,17 @@
+const divide = function (num = 2000, num2 = 100) {
+ return    num / num2;
+  
+  
+}
+
+const square = (num1) => num1 * num1; {
+
+   square (num1,num1);
+
+
+}
+function add(a, b) {
+
+    return a + b;
+}
+
